15464a18257fb7d44ea94d1405e1bee55aa442e4
Kalpesh Koli <kolikalpesh65@gmail.com>
2024-03-08 13:07:49 +0530

    last commit

--- data/database.js
+++ data/database.js
@@ -5,6 +5,6 @@ import mongoose from "mongoose";
 export const connectDB = ()=>{
     mongoose.connect(process.env.MONGO_URI,{
     dbName:"nodeapi"
-}).then(()=>console.log("databas cpnnected")).catch((e)=>console.log(e));
+}).then((c)=>console.log(`database connected : ${c.connection.host}`)).catch((e)=>console.log(e));
 
 }
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,5 @@
-import { User } from "../models/user.js";
 import jwt from 'jsonwebtoken';
+import { User } from "../models/user.js";
 
 export const isAuthenticated = async (req,res,next)=>{
     const {token}= req.cookies;
